f127e70145ef2b0069c3b43196b0a21735fd5c47
DavidMedin <davidmedin03@gmail.com>
2022-01-03 13:56:55 -0600

    added basic movement and gamepad joystick

--- player_controller.gd
+++ player_controller.gd
@@ -4,7 +4,7 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,5 +12,15 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	position += Vector2(speed * delta * Input.get_action_strength("move_right"),0)
+	position -= Vector2(speed * delta * Input.get_action_strength("move_left"),0)
+	position -= Vector2(0,speed * delta * Input.get_action_strength("move_up"))
+	position += Vector2(0,speed * delta * Input.get_action_strength("move_down"))
+	if Input.is_action_pressed("ui_down"):
+		position += Vector2(0,speed * delta)
+	if Input.is_action_pressed("ui_left"):
+		position -= Vector2(speed * delta,0)
+	if Input.is_action_pressed("ui_up"):
+		position -= Vector2(0,speed * delta)
+	
