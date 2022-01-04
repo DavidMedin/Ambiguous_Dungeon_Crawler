e89ce91d8a8c586a6f6e5b87ba91c8e13bd2a191
David Medin <davidmedin03@gmail.com>
2022-01-04 16:33:51 -0600

    rat rotates now

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,30 +1,20 @@
 extends KinematicBody2D
 
-var speed = 500
-var map : TileMap
+var speed = 2500
 const floor_index = 8 # index into tilemap
 
 var device_id = PlayerData.players[0].device
 
-func _ready():
-	map = get_node("/root/Node2D/TileMap")
-	if map == null:
-		printerr("Couldn't find TileMap in the scene tree!")
-
-# func _unhandled_input(event):
-# 	if event is InputEventJoypadButton:
-# 		if event.pressed and event.button_index == JOY_XBOX_A:
-# 			device_id = event.device
-# 			print("You are device: ",device_id)
-
 func _process(delta):
 	if device_id != -1:
-		var dead = 0.5
+		var dead = 0.25
 		var vel = Vector2(0,0)
-		var tmp : float = speed * delta * Input.get_joy_axis(device_id,JOY_AXIS_0)
-		if abs(tmp) > dead*speed*delta:
+		var tmp : float =  Input.get_joy_axis(device_id,JOY_AXIS_0)
+		if abs(tmp) > dead:
 			vel += Vector2(tmp,0)
-		tmp =  speed * delta * Input.get_joy_axis(device_id,JOY_AXIS_1)
-		if abs(tmp) > dead*speed*delta:
+		tmp =   Input.get_joy_axis(device_id,JOY_AXIS_1)
+		if abs(tmp) > dead:
 			vel += Vector2(0,tmp)
 		move_and_slide(vel * delta * speed)
+		if vel != Vector2(0,0):
+			rotation = vel.angle()
